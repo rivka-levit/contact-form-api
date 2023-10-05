65492f1973f9152e45e50908b2b1229eacdbb740
rivka-yuliia <juliacreadora@gmail.com>
2023-10-06 00:50:20 +0300

    implement profile api

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,11 +4,12 @@ URL mappings for user APIs.
 
 from django.urls import path
 
-from user.views import CreateUserView, AuthTokenView
+from user.views import CreateUserView, AuthTokenView, UserProfileView
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', AuthTokenView.as_view(), name='token'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -2,9 +2,13 @@
 Views for user APIs.
 """
 
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.settings import api_settings
+
+from rest_framework.permissions import IsAuthenticated
+from core.permissions import AccessOwnerOnly
 
 from user.serializers import UserSerializer, AuthTokenSerializer
 
@@ -20,3 +24,16 @@ class AuthTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileView(RetrieveUpdateDestroyAPIView):
+    """Retrieve, update or delete user profile."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, AccessOwnerOnly]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+
+        return self.request.user
