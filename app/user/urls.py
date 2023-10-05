012adb86bbd23f977d787da7985f0090bdbf7427
rivka-yuliia <juliacreadora@gmail.com>
2023-10-05 13:21:29 +0300

    implement auth token api

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,10 +4,11 @@ URL mappings for user APIs.
 
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, AuthTokenView
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
+    path('token/', AuthTokenView.as_view(), name='token'),
 ]
