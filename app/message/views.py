06bde5422feb09267ab75984d8f996766cadb17f
rivka-yuliia <juliacreadora@gmail.com>
2023-10-06 14:48:21 +0300

    extend schema descriptions

--- app/message/views.py
+++ app/message/views.py
@@ -1,12 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+    OpenApiParameter,
+    OpenApiTypes
+)
+
 from message.serializers import MessageSerializer, MessageDetailSerializer
 
 from core.models import Message
 from core.permissions import AccessOwnerOnly
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description='List of all the messages that are not in ban.'
+    ),
+    create=extend_schema(description='Create a new message in the system.'),
+    update=extend_schema(description='Full update of a message.'),
+    partial_update=extend_schema(description='Partial update of a message.'),
+    destroy=extend_schema(description='Remove a message from the system.'),
+)
 class MessageViewSet(ModelViewSet):
     """View for managing message APIs."""
 
